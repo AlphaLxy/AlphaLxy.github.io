f41511c4dddec04737d61909699e019699ff896a
AlphaLxy <liuxinyu0922@gmail.com>
2023-01-11 20:41:26 +0800

    Site updated: 2023-01-11 20:41:26

--- js/animation.js
+++ js/animation.js
@@ -43,8 +43,8 @@
                 setTimeout(() => {
                     element.style.opacity = '1';
                     element.style.transform = '';
-                    element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out, box-shadow 0.3s ease-in-out';
-                }, i * 100);
+                    element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
+                }, i * 30);
                 i++;
             });
         });
